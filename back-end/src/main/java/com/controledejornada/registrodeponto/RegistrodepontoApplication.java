8a57c00631df8a901286115436c31f1860d63092
Andre Cruz <andreldcruz@outlook.com>
2022-10-26 21:38:05 -0300

    Projeto iniciado

---
+++ back-end/src/main/java/com/controledejornada/registrodeponto/RegistrodepontoApplication.java
@@ -0,0 +1,13 @@
+package com.controledejornada.registrodeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrodepontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrodepontoApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/controledejornada/registrodeponto/RegistrodepontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controledejornada.registrodeponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrodepontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
